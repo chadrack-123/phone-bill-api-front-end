3ba901dff4d2356853dd0e24aad99869c5c1ccf6
Chadrak Mulamba <chadrackmulamba2@gmail.com>
2024-08-28 22:44:29 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,52 @@
+function phoneBillApp() {
+    return {
+        // Variables to hold data
+        newPlan: { name: '', call_cost: '', sms_cost: '' },
+        createStatus: '',
+        pricePlans: [],
+        selectedPlan: '',
+        actions: '',
+        totalBill: 0,
+
+        // Fetch all price plans
+        fetchPricePlans() {
+            fetch('/api/price_plans/')
+                .then(res => res.json())
+                .then(data => {
+                    this.pricePlans = data;
+                });
+        },
+
+        // Create a new price plan
+        createPricePlan() {
+            fetch('/api/price_plan/create', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(this.newPlan)
+            })
+            .then(res => res.text())
+            .then(data => {
+                this.createStatus = data;
+                this.newPlan = { name: '', call_cost: '', sms_cost: '' }; // Clear the form
+                this.fetchPricePlans(); // Refresh the list of price plans
+            });
+        },
+
+        // Calculate phone bill
+        calculatePhoneBill() {
+            const billData = {
+                price_plan: this.selectedPlan,
+                actions: this.actions
+            };
+            fetch('/api/phonebill', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(billData)
+            })
+            .then(res => res.json())
+            .then(data => {
+                this.totalBill = data.total;
+            });
+        }
+    };
+}
